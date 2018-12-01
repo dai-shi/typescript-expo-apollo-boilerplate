e09ea56ad22a44249c83e0654a2ed8e68be8bf19
daishi <daishi@axlight.com>
2018-12-01 23:30:25 +0900

    add jest test

---
+++ src/App.spec.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import App from './App';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('App', () => {
+  it('snapshot test', async () => {
+    const tree = renderer.create(
+      <App skipLoadingScreen={true} />,
+    ).toJSON();
+    await sleep(1);
+    expect(tree).toMatchSnapshot();
+  });
+});
