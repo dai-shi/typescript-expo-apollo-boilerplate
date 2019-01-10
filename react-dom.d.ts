6d1cf90049a5c57e16a0caa9598314ed5605e4aa
daishi <daishi@axlight.com>
2019-01-10 23:21:55 +0900

    remove no longer used file

--- react-dom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-dom/server' {
-  import { ReactElement } from 'react';
-
-  // tslint:disable-next-line:no-any
-  export function renderToStaticMarkup(element: ReactElement<any>): string;
-}
